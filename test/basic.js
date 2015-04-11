c6b06270beb3e831069550b05c77d64ac117d30f
Feross Aboukhadijeh <feross@feross.org>
2015-04-11 15:12:43 +1200

    test that `req` is returned

--- test/basic.js
+++ test/basic.js
@@ -316,3 +316,25 @@ test('get.concat', function (t) {
     })
   })
 })
+
+test('access `req` object', function (t) {
+  t.plan(2)
+
+  var server = http.createServer(function (req, res) {
+    res.statusCode = 200
+    res.end('response')
+  })
+
+  server.listen(0, function () {
+    var port = server.address().port
+    var req = get('http://localhost:' + port, function (err, res) {
+      t.error(err)
+      res.resume() // discard data
+      server.close()
+    })
+
+    req.on('socket', function () {
+      t.pass('got `socket` event')
+    })
+  })
+})
