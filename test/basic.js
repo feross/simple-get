ec6570d672cb431d2739637500d958c6cb343e66
Tim Kuijsten <tim@netsend.nl>
2016-04-30 16:42:32 +0200

    support basic auth

--- test/basic.js
+++ test/basic.js
@@ -31,6 +31,28 @@ test('simple get', function (t) {
   })
 })
 
+test('basic auth', function (t) {
+  t.plan(4)
+
+  var server = http.createServer(function (req, res) {
+    t.equal(req.headers.authorization, 'Basic Zm9vOmJhcg==')
+    res.statusCode = 200
+    res.end('response')
+  })
+
+  server.listen(0, function () {
+    var port = server.address().port
+    get('http://foo:bar@localhost:' + port, function (err, res) {
+      t.error(err)
+      t.equal(res.statusCode, 200)
+      res.pipe(concat(function (data) {
+        t.equal(data.toString(), 'response')
+        server.close()
+      }))
+    })
+  })
+})
+
 test('follow redirects (up to 10)', function (t) {
   t.plan(13)
 
