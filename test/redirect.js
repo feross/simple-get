ffdd67488ae70131dcb167d0d26c72c1871389d3
Feross Aboukhadijeh <feross@feross.org>
2018-04-24 12:37:17 -0700

    test: Add failing test for #32
    https://github.com/feross/simple-get/issues/32

--- test/redirect.js
+++ test/redirect.js
@@ -164,3 +164,94 @@ test('redirect http to https', function (t) {
     })
   })
 })
+
+test('redirect to different host/port', function (t) {
+  t.plan(7)
+
+  var port1 = null
+  var port2 = null
+
+  var server1 = http.createServer(function (req, res) {
+    t.equal(req.url, '/path1')
+    res.statusCode = 301
+    // Redirect from localhost:port1 to 127.0.0.1:port2 (different host and port!)
+    res.setHeader('Location', 'http://127.0.0.1:' + port2 + '/path2')
+    res.end()
+  })
+
+  var server2 = http.createServer(function (req, res) {
+    t.equal(req.url, '/path2')
+    // Confirm that request was made with new host and port (127.0.0.1:port2)
+    t.equal(req.headers.host, `127.0.0.1:${port2}`)
+    res.statusCode = 200
+    res.end('response')
+  })
+
+  server1.listen(0, function () {
+    port1 = server1.address().port
+    server2.listen(0, function () {
+      port2 = server2.address().port
+      console.log(port1, port2)
+      get('http://localhost:' + port1 + '/path1', function (err, res) {
+        t.error(err)
+        t.equal(res.statusCode, 200)
+        concat(res, function (err, data) {
+          t.error(err)
+          t.equal(data.toString(), 'response')
+          server1.close()
+          server2.close()
+        })
+      })
+    })
+  })
+})
+
+// See https://github.com/feross/simple-get/issues/32
+test('redirect should clear explicitly specified `host` header', function (t) {
+  t.plan(8)
+
+  var port1 = null
+  var port2 = null
+
+  var server1 = http.createServer(function (req, res) {
+    t.equal(req.url, '/path1')
+    t.equal(req.headers.host, `localhost:${port1}`)
+    res.statusCode = 301
+    // Redirect from localhost:port1 to 127.0.0.1:port2 (different host and port!)
+    res.setHeader('Location', 'http://127.0.0.1:' + port2 + '/path2')
+    res.end()
+  })
+
+  var server2 = http.createServer(function (req, res) {
+    t.equal(req.url, '/path2')
+    // Confirm that request was made with new host and port (127.0.0.1:port2), i.e.
+    // that the explicitly specified `Host` header was cleared upon redirect.
+    t.equal(req.headers.host, `127.0.0.1:${port2}`)
+    res.statusCode = 200
+    res.end('response')
+  })
+
+  server1.listen(0, function () {
+    port1 = server1.address().port
+    server2.listen(0, function () {
+      port2 = server2.address().port
+      console.log(port1, port2)
+      get({
+        url: `http://localhost:${port1}/path1`,
+        // Explicitly specify a `Host` header, so it won't be set automatically
+        headers: {
+          host: `localhost:${port1}`
+        }
+      }, function (err, res) {
+        t.error(err)
+        t.equal(res.statusCode, 200)
+        concat(res, function (err, data) {
+          t.error(err)
+          t.equal(data.toString(), 'response')
+          server1.close()
+          server2.close()
+        })
+      })
+    })
+  })
+})
