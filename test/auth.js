1b902cf9c2e88466db29d0dbd2e4e0bd5213471a
Linus Unnebäck <linus@folkdatorn.se>
2022-02-06 15:38:38 +0100

    Add more test cases for basic auth

--- test/auth.js
+++ test/auth.js
@@ -25,3 +25,49 @@ test('basic auth', function (t) {
     })
   })
 })
+
+test('basic auth + host', function (t) {
+  t.plan(5)
+
+  const server = http.createServer(function (req, res) {
+    t.equal(req.headers.authorization, 'Basic Zm9vOmJhcg==')
+    res.statusCode = 200
+    res.end('response')
+  })
+
+  server.listen(0, function () {
+    const port = server.address().port
+    get({ auth: 'foo:bar', host: 'localhost', port }, function (err, res) {
+      t.error(err)
+      t.equal(res.statusCode, 200)
+      concat(res, function (err, data) {
+        t.error(err)
+        t.equal(data.toString(), 'response')
+        server.close()
+      })
+    })
+  })
+})
+
+test('basic auth + hostname', function (t) {
+  t.plan(5)
+
+  const server = http.createServer(function (req, res) {
+    t.equal(req.headers.authorization, 'Basic Zm9vOmJhcg==')
+    res.statusCode = 200
+    res.end('response')
+  })
+
+  server.listen(0, function () {
+    const port = server.address().port
+    get({ auth: 'foo:bar', hostname: 'localhost', port }, function (err, res) {
+      t.error(err)
+      t.equal(res.statusCode, 200)
+      concat(res, function (err, data) {
+        t.error(err)
+        t.equal(data.toString(), 'response')
+        server.close()
+      })
+    })
+  })
+})
